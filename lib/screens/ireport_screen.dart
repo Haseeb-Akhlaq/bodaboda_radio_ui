e5f61d043e5ab8d0765acfb935aae226804acf3c
haseeb akhlaq <haseebakhlaq2000@gmail.com>
2021-10-31 21:15:23 +0500

    done

--- lib/screens/ireport_screen.dart
+++ lib/screens/ireport_screen.dart
@@ -1,77 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class IReportScreen extends StatelessWidget {
   const IReportScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Container(
-          color: Colors.black,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SvgPicture.asset(
-                      'assets/images/notification.svg',
-                      color: Colors.transparent,
-                    ),
-                    Image.asset('assets/images/logo.png'),
-                    const Icon(
-                      Icons.search,
-                      color: Colors.white,
-                      size: 28,
-                    ),
-                  ],
+    return Container(
+      color: const Color(0xffC00010),
+      child: ListView(
+        children: [
+          SizedBox(height: 2),
+          Image.asset('assets/images/iReport .jpeg'),
+          SizedBox(height: 2),
+          Stack(
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                child: Image.asset(
+                  'assets/images/i.png',
+                  fit: BoxFit.cover,
                 ),
-              ],
-            ),
-          ),
-        ),
-        Image.asset('assets/images/i.png'),
-        Expanded(
-          child: Container(
-            width: double.infinity,
-            color: const Color(0xff2E2E2E),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              Positioned(
+                bottom: 15,
+                left: MediaQuery.of(context).size.width * 0.3,
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 5,
+                  ),
+                  color: Colors.white,
+                  child: Column(
                     children: const [
                       Text(
-                        'Boda-Boda Radio iReport',
+                        'Just',
                         style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'Shoot and Send',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
                         ),
                       ),
                     ],
                   ),
-                  SizedBox(height: 10),
-                  const Text(
-                    'Duis rhoncus dui venenatis consequat porttitor. Etiam aliquet congue consequat. In posuere, nunc sit amet laoreet blandit, urna sapien imperdiet lectus, et molestie sem tortor quis dui. Donec molestie nisi iaculis sodales mollis. Nullam non tellus sed elit pulvinar dignissim ut vel ex. Phasellus at leo sed est egestas posuere eu eget magna.',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                  const SizedBox(height: 25),
+                ),
+              )
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            color: const Color(0xffC00010),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 5),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
                         alignment: Alignment.center,
                         width: 130,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Not Now',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        width: 140,
                         height: 40,
                         decoration: BoxDecoration(
                           color: const Color(0xffF15A26),
@@ -80,10 +94,10 @@ class IReportScreen extends StatelessWidget {
                         child: const Padding(
                           padding: EdgeInsets.all(10),
                           child: Text(
-                            'Next',
+                            'Yes I\'m in',
                             style: TextStyle(
                               color: Colors.white,
-                              fontSize: 14,
+                              fontSize: 18,
                             ),
                           ),
                         ),
@@ -94,8 +108,9 @@ class IReportScreen extends StatelessWidget {
               ),
             ),
           ),
-        )
-      ],
+          SizedBox(height: 12),
+        ],
+      ),
     );
   }
 }
